7072bf758fb5a73eb2f21ac08f45eeea36a123c1
Albert Arrieta <albert.arrieta12@gmail.com>
2022-05-01 15:52:18 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header 
+      className="App-header" 
+      style={darkMode ? {backgroundColor: '#ebedf0'} :
+       {backgroundColor: '#18191a'}}>
+        <h1 className={darkMode ? 'text-dark' : 'text-light'}>Hello World!</h1>
+        <button 
+        className={darkMode ? 'btn btn-dark' : 'btn btn-light'}
+        onClick={() => setDarkMode(!darkMode)}
+        >Dark mode!</button>
       </header>
     </div>
   );
